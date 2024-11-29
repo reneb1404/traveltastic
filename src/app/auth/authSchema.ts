a20b54c39fe816fc16632733a1e64dffbe7aa6f6
Rene B. <133756710+Krypto1337@users.noreply.github.com>
2024-11-29 11:51:47 +0100

    Added supabase auth methods

---
+++ src/app/auth/authSchema.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+
+export const loginSchema = z.object({
+	email: z
+		.string({ required_error: "Email is required" })
+		.min(1, "Email is required")
+		.email("Invalid email"),
+	password: z
+		.string({ required_error: "Password is required" })
+		.min(1, "Password is required"),
+});
+
+export const signupSchema = z
+	.object({
+		email: z.string().email("Invalid email address"),
+		password: z.string().min(8, "Password must be at least 8 characters long"),
+		confirmPassword: z.string(),
+	})
+	.refine((data) => data.password === data.confirmPassword, {
+		message: "Passwords do not match",
+		path: ["confirmPassword"],
+	});
